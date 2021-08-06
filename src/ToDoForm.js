69269bdbfbdcb43a508a070737338d8679a0e86f
chirptotheeast <alyssa.easterly@yahoo.com>
2021-08-06 16:40:23 -0400

    able to add todo

---
+++ src/ToDoForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+const ToDoForm = ({addTask}) => {
+const [ userInput, setUserInput ] = useState('');
+
+const handleChange = (e) => {
+    setUserInput(e.currentTarget.value)
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(userInput);
+    setUserInput("");
+}
+
+
+
+return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
+            <button>Submit</button>
+        </form>
+
+    </div>
+)
+
+}
+
+export default ToDoForm
